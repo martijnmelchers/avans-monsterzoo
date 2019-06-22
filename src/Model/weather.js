5923bb2b7d1f0152f93550cd43888627b70816e8
SaschaMendel <saschamendel2016@gmail.com>
2019-06-22 21:07:43 +0200

    Changed temperature units to metric from kelvin

--- src/Model/weather.js
+++ src/Model/weather.js
@@ -1,7 +1,7 @@
 export class WeatherModel {
     getWeather(loc, callback){
         let xmlHttp = new XMLHttpRequest();
-        let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=93c1bff2d02ce75777a3fcc1bdf35da7`;
+        let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=93c1bff2d02ce75777a3fcc1bdf35da7&units=metric`;
         fetch(requestUrl).then(function(response){
             return response.json();
         }).then(function(json){

5ea48b6503113219fdca4c2b774438b22bf75ab5
SaschaMendel <saschamendel2016@gmail.com>
2019-06-23 00:15:31 +0200

    added some functions for monster moving and created some graphics.

--- src/Model/grid.js
+++ src/Model/grid.js
@@ -8,12 +8,31 @@ export class GridModel {
         this.rows = Config.grid.rows;
         this.columns = Config.grid.columns;
         this.gridLayout = Storage.loadLayout(this.region);
-
+        this.monsterLayout =  [];
         console.log(JSON.stringify(this))
     }
 
     isDisabled(x, y) {
         return this.gridLayout["layout"][x][y] === 1;
+    }
+
+    setMonster(x,y, monster){
+        this.monsterLayout[x][y] = monster;
+    }
+
+    getMonster(x,y){
+        return this.monsterLayout[x][y];
+    }
+
+    moveMonster(fromX, fromY, toX, toY){
+        if(this.getMonster(toX,toY) === null){
+            // Grab the instance of the position from.
+            let monster = this.getMonster(fromX,fromY);
+            this.setMonster(toX,toY, monster);
+
+            // Clear the previous spot.
+            this.setMonster(fromX, fromY, null);
+        }        
     }
 }
 
---
+++ src/Model/monster.js
@@ -0,0 +1,37 @@
+class Monster {
+    constructor(){
+        this.name = "Monster";
+        this.element = "Monster";
+        this.furType = "";
+        this.numArms = 0;
+        this.numLegs = 0;
+        this.canSwim = false;     
+        this.canFly = true;
+        this.color = "";
+    }
+
+    screech(){
+        // TODO: Play sound or change color.
+    }
+    
+    // Check if the monster has a valid setup.
+    verify(){
+        // TODO
+    }
+    
+    configure(properties){
+        for (const prop in properties) {
+            if (properties.hasOwnProperty(prop)) {
+                const element = properties[prop];
+                this[prop] = element;
+            }
+        }
+    }
+}
+
+
+class MonsterModel {
+    createMonster(){
+        
+    }  
+}
